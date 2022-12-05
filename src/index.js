fdfaaabd7f5a4162ef3a5e036fbba22ad19b1c7a
TheLegendaryWarrior <sanket.potdar563@gmail.com>
2022-12-05 06:25:41 +0530

    Added GitHub Actions

--- src/index.js
+++ src/index.js
@@ -2,11 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { ChakraProvider } from "@chakra-ui/react";
+import { extendTheme, ChakraProvider } from "@chakra-ui/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const theme = extendTheme({
+  initialColorMode: "dark",
+  useSystemColorMode: false
+});
+
 root.render(
-  <ChakraProvider>
+  <ChakraProvider theme={theme}>
     <App />
   </ChakraProvider>
 );
